a09a1bd6b5b375778e88c38d941a31388c247314
Pronkle <allenleiguo@gmail.com>
2025-03-10 18:43:01 -0500

    some comments

--- src/main/java/frc/robot/commands/autodispense.java
+++ src/main/java/frc/robot/commands/autodispense.java
@@ -8,27 +8,32 @@ public class autodispense extends Command{
     private feeder dispenser;
     private Timer timer;
 
+    // Constructor
     public autodispense(Timer t, feeder d){
         this.dispenser = d;
         this.timer = t;
     }
 
+    // starting code at start of call per use case
     @Override
     public void initialize(){
         timer.reset();
-        timer.start();
+        timer.start(    );
     }
 
+    // actual command
     @Override
     public void execute(){
         dispenser.dispense();
     }
 
+    // in case of an interuption if interrupted boolean changed will halt action
     @Override
     public void end(boolean interrupted){
         dispenser.stop();
     }
 
+    // Expected completion time of 1 second, ends command
     @Override
     public boolean isFinished(){
         return timer.hasElapsed(1);
