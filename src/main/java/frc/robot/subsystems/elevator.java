b90671220d0dd0df9d8312051eb4d75999fd67ca
Pronkle <allenleiguo@gmail.com>
2025-03-18 17:43:03 -0500

    refactoring changes

--- src/main/java/frc/robot/subsystems/elevator.java
+++ src/main/java/frc/robot/subsystems/elevator.java
@@ -8,12 +8,15 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 
 
-public class elevatorDOWN extends ProfiledPIDController {
+public class elevator extends ProfiledPIDController {
     public TalonFX GB_R = new TalonFX(1);
     public TalonFX GB_L = new TalonFX(1);
 
     private final ElevatorFeedforward m_elevatorFF =
-    new ElevatorFeedforward(
-        pivotUpConstants.kSVolts, pivotUpConstants.kGVolts,
-        pivotUpConstants.kVVoltSecondPerRad, pivotUpConstants.kAVoltSecondSquaredPerRad);
+    new ElevatorFeedforward(0, 0, 0, 0);
+    // Substitute values, exchange when available
+
+    public elevator(){
+        
+    }
 }
