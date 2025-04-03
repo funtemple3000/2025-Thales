8e489de1ef8f76458abccc3d6b19575d57b7f5ff
Pronkle <allenleiguo@gmail.com>
2025-04-03 13:51:15 -0400

    elevator timing reintegration

--- src/main/java/frc/robot/commands/autoL3up.java
+++ src/main/java/frc/robot/commands/autoL3up.java
@@ -1,22 +1,32 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator_PS;
 
 public class autoL3up extends Command {
     private elevator_PS m_elevator;
+    private Timer timer;
 
     public autoL3up(elevator_PS L){
         this.m_elevator = L;
+        this.timer = new Timer();
     }
 
     @Override
     public void initialize(){
+        timer.reset();
+        timer.start();
     }
 
     @Override
     @SuppressWarnings("removal")
     public void execute(){
         m_elevator.setGoal(-26);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return timer.hasElapsed(2.5);
     }
 }
